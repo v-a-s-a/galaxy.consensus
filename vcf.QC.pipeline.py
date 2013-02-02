07b5158a76c9789362e2e56abcf24d35450b4204
trubetskoy.vasa@gmail.com <trubetskoy.vasa@gmail.com>
2013-02-02 13:54:48 -0600

    initial. added high level overview.

---
+++ vcf.QC.pipeline.py
@@ -0,0 +1,48 @@
+'''
+A pipeline for generating a set of QC metrics for an arbitrary VCF.
+
+Target Metrics:
+    Current:
+        * ts/tv ratio
+        * mendelian error
+        * sample missingness rate
+        * site missingness rate
+        * sample non-consensus rate
+        * site non-consensus rate
+        * MAF distribution
+
+    Planned:
+        * combined QUAL
+
+programs used:
+    * vcftools
+    * PLINK
+
+
+'''
+
+import vcf
+import optparse as opt
+
+
+## CLASSES
+
+## FUNCTIONS
+
+
+
+## MAIN
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
