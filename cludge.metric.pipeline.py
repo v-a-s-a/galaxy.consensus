69e20db863aef46d04a50d9fc19ce3ee429d75a8
trubetskoy.vasa@gmail.com <vasya@login-0-0.local>
2013-02-05 23:27:55 -0600

    Cludge pipeline functioning for all three branches

--- cludge.metric.pipeline.py
+++ cludge.metric.pipeline.py
@@ -6,114 +6,164 @@ vcf.
 '''
 
 import subprocess as sp
+import gzip as gz
+import optparse as opt
 
-devnull = open('/dev/null', 'w')
-vcfFile = './data/ATLAS.merged.ontarget.chr22.SM.vcf'
-sampleKeyFile = '/nas40t2/tsExoms/Sample Info/Scharf_master_sequencing_sample_key.csv'
-pedFile = '/nas40t2/tsExoms/Sample Info/updated_Scharf_Pedigree.csv'
-intermedBase = 'temp'
+def smart_open(f):
+    '''
+    Open compressed files if compressed
+    '''
+    if f.endswith('.gz'):
+        return gz.open(f)
+    else:
+        return open(f)
+
+def smart_vcftools(call):
+    '''
+    Analog to smart open for calling vcftools with a gzipped vcf
+    '''
+    for idx, element in enumerate(call):
+        if element.endswith('.vcf.gz'):
+            print element
+            call[idx-1] = '--gzvcf'
+    return call
+
+def main():
+
+    ## parse command line arguments
+    parser = opt.OptionParser()
+    parser.add_option('--vcf', dest = 'vcfFile', action = 'store', 
+        help = 'File path for vcf for which to generate QC metrics.')
+    parser.add_option('--intermed-base', dest = 'intermedBase', action = 'store', 
+        help = 'File path base for storage of intermediate files.')
+    (options, args) = parser.parse_args()
+
+    vcfFile = options.vcfFile
+    intermedBase = options.intermedBase
+
+    devnull = open('/dev/null', 'w')
+    sampleKeyFile = '/nas40t2/tsExoms/Sample Info/Scharf_master_sequencing_sample_key.csv'
+    pedFile = '/nas40t2/tsExoms/Sample Info/updated_Scharf_Pedigree.csv'
 
 
-### get a MAF distribution
-#mafCall = ['vcftools', '--vcf', vcfFile, '--freq', '--out', intermedBase] 
-##print ' '.join(mafCall)
-#sp.call(mafCall)
-#
-### get the ts/tv ratio
-#tstvCall = ['vcftools', '--vcf', vcfFile, '--TsTv', '1' , '--out', intermedBase]
-##print ' '.join(tstvCall)
-#sp.call(tstvCall)
-#
-### get the missingness rates
-#missCall = ['vcftools', '--vcf', vcfFile, '--missing', '--out', intermedBase]
-##print ' '.join(missCall)
-#sp.call(missCall)
-#
-#### mendelian errors using plink
-#
-### convert vcf to plink formatted files
-#convertCall = ['vcftools', '--vcf', vcfFile, '--plink', '--recode', '--out',
-#    intermedBase]
-#sp.call(convertCall)
 
-## store mapping of SM to RG
-rg_sm = dict()
-fconn = open(sampleKeyFile)
-header = fconn.next()
-for line in fconn:
-   line = line.split(',')
-   rg_sm[line[1]]  = line[0]
-fconn.close()
+    ## identify the total number of variants in the VCF file
+    zgrepCall = ['zgrep', '-v', '\'#\'', vcfFile]
+    wcCall = ['wc', '-l']
+    zgrep = sp.Popen( zgrepCall, stdout = sp.PIPE )
+    wc = sp.Popen( wcCall, stdin = zgrep.stdout, stdout = sp.PIPE )
+    zgrep.stdout.close()
+    nsnp = wc.communicate()[0]
 
-## add pedigree information to plink ped file file
-mothers = dict()
-fathers = dict()
-families = dict()
-sexes = dict()
-with open(pedFile) as fconn:
-    header = enumerate(fconn.next().split(','))
-    index = dict( ( (x[1], x[0]) for x in header ) )
+
+    ## get the ts/tv ratio
+    tstvCall = ['vcftools', '--vcf', vcfFile, '--TsTv', nsnp, '--out',
+        intermedBase + '.tstv']
+    tstvCall = smart_vcftools(tstvCall)
+    #print ' '.join(tstvCall)
+    sp.call(tstvCall)
+
+    ## get the missingness rates
+    missCall = ['vcftools', '--vcf', vcfFile, '--missing', '--out', intermedBase]
+    missCall = smart_vcftools(missCall)
+    #print ' '.join(missCall)
+    sp.call(missCall)
+
+    ### mendelian errors using plink
+
+    ## convert vcf to plink formatted files
+    convertCall = ['vcftools', '--vcf', vcfFile, '--plink', '--recode', '--out',
+        intermedBase]
+    convertCall = smart_vcftools(convertCall)
+    sp.call(convertCall)
+
+    ## store mapping of SM to RG
+    rg_sm = dict()
+    fconn = smart_open(sampleKeyFile)
+    header = fconn.next()
     for line in fconn:
-        line = line.split(',')
-        if rg_sm.get(line[index['Mother']]):
-            mother = rg_sm[ line[index['Mother']] ]
-        else:
-            mother = line[index['Mother']]
+       line = line.split(',')
+       rg_sm[line[1]]  = line[0]
+    fconn.close()
 
-        if rg_sm.get(line[index['Father']]):
-            father = rg_sm[ line[index['Father']]]
-        else:
-            father = line[index['Father']]
-        
-        if mother=='Not Sequenced':
-            mother = '0'
-        if father=='Not Sequenced':
-            father = '0'
+    ## add pedigree information to plink ped file file
+    mothers = dict()
+    fathers = dict()
+    families = dict()
+    sexes = dict()
+    with smart_open(pedFile) as fconn:
+        header = enumerate(fconn.next().split(','))
+        index = dict( ( (x[1], x[0]) for x in header ) )
+        for line in fconn:
+            line = line.split(',')
+            if rg_sm.get(line[index['Mother']]):
+                mother = rg_sm[ line[index['Mother']] ]
+            else:
+                mother = line[index['Mother']]
 
-        fathers[line[index['SM_tag (Local_ID)']]] = father
-        mothers[line[index['SM_tag (Local_ID)']]] = mother
+            if rg_sm.get(line[index['Father']]):
+                father = rg_sm[ line[index['Father']]]
+            else:
+                father = line[index['Father']]
+            
+            if mother=='Not Sequenced':
+                mother = '0'
+            if father=='Not Sequenced':
+                father = '0'
 
-        if rg_sm.get(line[index['Individ']]):
-            family = line[index['Family']]
-        else:
-            family = line[index['SM_tag (Local_ID)']] 
+            fathers[line[index['SM_tag (Local_ID)']]] = father
+            mothers[line[index['SM_tag (Local_ID)']]] = mother
 
-        families[line[index['SM_tag (Local_ID)']]] = family
+            if rg_sm.get(line[index['Individ']]):
+                family = line[index['Family']]
+            else:
+                family = line[index['SM_tag (Local_ID)']] 
 
-        sexes[line[index['SM_tag (Local_ID)']]] = line[index['Sex']]
+            families[line[index['SM_tag (Local_ID)']]] = family
+
+            sexes[line[index['SM_tag (Local_ID)']]] = line[index['Sex']]
 
 
-newPlinkFile = intermedBase + '.pedigree.ped'
-newPlinkConn = open(newPlinkFile, 'w')
-with open(intermedBase + '.ped') as plinkFile:
-    for line in plinkFile:
-        line = line.strip().split('\t')
-        iid = line[1]
-        if fathers.get(iid):
-            line[2] = fathers[iid]
-        if mothers.get(iid):
-            line[3] = mothers[iid]
-        if families.get(iid):
-            line[0] = families[iid]
-        if sexes.get(iid):
-            line[4] = sexes[iid] 
-        ## add a fake phenotype
-        line[5] = '2'
-        print >> newPlinkConn, '\t'.join(line)
-newPlinkConn.close()
+    newPlinkFile = intermedBase + '.pedigree.ped'
+    newPlinkConn = open(newPlinkFile, 'w')
+    with smart_open(intermedBase + '.ped') as plinkFile:
+        for line in plinkFile:
+            line = line.strip().split('\t')
+            iid = line[1]
+            if fathers.get(iid):
+                line[2] = fathers[iid]
+            if mothers.get(iid):
+                line[3] = mothers[iid]
+            if families.get(iid):
+                line[0] = families[iid]
+            if sexes.get(iid):
+                line[4] = sexes[iid] 
+            ## add a fake phenotype
+            line[5] = '2'
+            print >> newPlinkConn, '\t'.join(line)
+    newPlinkConn.close()
 
-## calculate mendelian errors
-mendelCall = ['plink',
-    '--ped',
-    newPlinkFile,
-    '--map',
-    intermedBase + '.map',
-    '--mendel',
+    ## calculate mendelian errors
+    mendelCall = ['plink',
+        '--ped', newPlinkFile,
+        '--map', intermedBase + '.map',
+        '--mendel',
+        '--allow-no-sex',
+        '--out', intermedBase]
+    sp.call(mendelCall)# stdout = devnull, stderr = devnull)
+
+    ## generate minor allele frequency distribution
+    mafCall = ['plink',
+    '--ped', newPlinkFile,
+    '--map', intermedBase + '.map',
+    '--freq',
     '--allow-no-sex',
-    '--out',
-    intermedBase]
-sp.call(mendelCall)#, stdout = devnull, stderr = devnull)
+    '--out', intermedBase]
+    sp.call(mafCall)
+
+    devnull.close()
 
 
 
-devnull.close()
+if __name__ == '__main__':
+    main()
