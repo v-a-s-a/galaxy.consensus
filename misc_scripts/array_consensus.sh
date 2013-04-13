220488f86ae37bd248ced56c1c400e0add17faca
vtrubets <trubetskoy.vasa@gmail.com>
2013-04-13 12:51:41 -0500

    Did some new consensus calling

--- misc_scripts/array_consensus.sh
+++ misc_scripts/array_consensus.sh
@@ -2,13 +2,14 @@
 #$ -V
 #$ -cwd
 #$ -S /bin/bash
-#$ -N arr.consensus
+#$ -N strict.consensus
 #$ -t 1-22
 
 
-time python consensus.py --out ts_exomes_data/consensus.chr${SGE_TASK_ID}.vcf \
-    --atlas-vcf ts_exomes_data/atlas_exome_chr${SGE_TASK_ID}.recode.vcf \
-    --freebayes-vcf ts_exomes_data/freebayes_03-25_exome_minQ_chr${SGE_TASK_ID}.recode.vcf \
-    --gatk-vcf ts_exomes_data/gatk_05-05_exome_chr${SGE_TASK_ID}.recode.vcf \
-    --db-file ~/tmp/consensus.${SGE_TASK_ID}.sqlite
+time python consensus_tool/consensus.py --out ts_exomes_data/consensus.consensus.strict.chr${SGE_TASK_ID}.vcf \
+    --atlas-vcf ts_exomes_data/atlas_branch/atlas_exome_chr${SGE_TASK_ID}.recode.vcf \
+    --freebayes-vcf ts_exomes_data/freebayes_branch/freebayes_03-25_exome_minQ_chr${SGE_TASK_ID}.recode.vcf \
+    --gatk-vcf ts_exomes_data/gatk_branch/gatk_05-05_exome_chr${SGE_TASK_ID}.recode.vcf \
+    --consensus-thresh 3 \
+    --db-file ~/tmp/strict.consensus.${SGE_TASK_ID}.sqlite
 
