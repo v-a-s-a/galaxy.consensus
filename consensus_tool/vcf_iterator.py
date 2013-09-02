8d7d9c19a0e95384e5163204bd5cc822b890578e
vtrubets <trubetskoy.vasa@gmail.com>
2013-09-02 13:22:49 -0500

    update to the vcf iterator

--- consensus_tool/vcf_iterator.py
+++ consensus_tool/vcf_iterator.py
@@ -18,21 +18,45 @@ class concordant_walker:
   def __init__(self, *args, **kwargs):
     self.vcfs = kwargs.get('vcfList')
     self.readers = [ pyvcf.Reader(open(x)) for x in self.vcfs ]
-    
-    print self.vcfs
+    ## first records in each file
+    self.inits = [ x.next() for x in self.readers ] 
+    ## track current positions of each record
+    self.positions = [ int(rec.POS) for rec in self.inits  ]
+    ## track current greatest position
+    self.prime = max(self.positions)
    
     ## TODO: check that files are sorted
     for vcf in self.vcfs:
       pass
 
+    def __updatePositions(self):
+      '''
+      Update the positions vector until all positions are equal, or
+      a new prime position is found.
+      '''
+      
+      
+      for reader in self.readers:
+        
+
 
   def walk_concordant_sites(self):
     '''
     Return variants which match across call sets as you walk files
-    '''
+    
+    Start simple:
+      * assume vcf is sorted
+      * assume only SNPs
+      * assume only bi-allelic variation
+      * assume only one chromosome
 
-    inits = [ x.next() for x in self.readers ] 
-    return inits
+    '''
+    ## find largest site among these, X
+    ## iterate other files until records match or are exceeded
+    ## if records match -- do consensus work/exit/whatever
+    ## if record found exceeding position of X, set this as X and iterate
+
+    return positions, prime
 
 
 def __main__():
