a2882ca99cb3dc68cae146123d31afdc8b9a0183
vtrubets <trubetskoy.vasa@gmail.com>
2013-09-02 15:41:34 -0500

    Can now walk concordant sites.

--- consensus_tool/vcf_iterator.py
+++ consensus_tool/vcf_iterator.py
@@ -1,11 +1,6 @@
 '''
 Given a list of vcf files, iterate over records in each simultaneously.
 
-Check that files are sorted.
-
-
-
-
 ** refactor this into a class with an __iter__ method w/ yields
 ** use the tabix indexing to access regions of files
 
@@ -18,26 +13,53 @@ class concordant_walker:
   def __init__(self, *args, **kwargs):
     self.vcfs = kwargs.get('vcfList')
     self.readers = [ pyvcf.Reader(open(x)) for x in self.vcfs ]
+    ## number of readers passed to this instance
+    self.vcfCount = len(self.vcfs)
     ## first records in each file
     self.inits = [ x.next() for x in self.readers ] 
     ## track current positions of each record
     self.positions = [ int(rec.POS) for rec in self.inits  ]
-    ## track current greatest position
+    ## track current greatest position (known as the prime position, and prime readers)
     self.prime = max(self.positions)
-   
+    ## pull inidices of readers which have reached the largest observed position
+    self.primeIndices = [ i for i in range(self.vcfCount) if self.positions[i]==self.prime ]
+
     ## TODO: check that files are sorted
     for vcf in self.vcfs:
       pass
 
-    def __updatePositions(self):
-      '''
-      Update the positions vector until all positions are equal, or
-      a new prime position is found.
-      '''
-      
-      
-      for reader in self.readers:
-        
+  def __updatePrimeIndices(self):
+    '''
+    Update indices of VCF readers which match the current largest position.
+    '''
+    self.primeIndices = [ i for i, x in enumerate(self.positions) if x==self.prime ]
+
+
+  def __updatePositions(self):
+    '''
+    Iterate non-prime readers.
+    '''
+    ## pull indices of readers which need to iterate to reach or exceed the prime position 
+    iterIndices = [ i for i in range(self.vcfCount) if i not in self.primeIndices ] 
+    ## update positions in non-prime readers
+    self.positions = [ x if i in self.primeIndices else int(self.readers[i].next().POS) for i,x in enumerate(self.positions) ]
+    ## update prime if necessary
+    if max(self.positions) > self.prime:
+      self.prime = max(self.positions)
+    ## update which readers we are iterating over
+    self.__updatePrimeIndices()
+
+
+
+    print self.positions, self.primeIndices
+
+  def __iterUntilEqual(self):
+    '''
+    Update the positions vector until all positions are equal.
+    '''
+    ## check if all readers have reached the same position
+    while len(set(self.positions)) != 1:
+      self.__updatePositions()
 
 
   def walk_concordant_sites(self):
@@ -49,14 +71,14 @@ class concordant_walker:
       * assume only SNPs
       * assume only bi-allelic variation
       * assume only one chromosome
-
     '''
     ## find largest site among these, X
     ## iterate other files until records match or are exceeded
     ## if records match -- do consensus work/exit/whatever
     ## if record found exceeding position of X, set this as X and iterate
 
-    return positions, prime
+    self.__iterUntilEqual()
+    return self.positions
 
 
 def __main__():
