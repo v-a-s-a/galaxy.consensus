bd50c12dc5c1c4d0445f177552f7377559292c22
vtrubets <trubetskoy.vasa@gmail.com>
2013-09-23 15:37:39 -0500

    Merged branches -- have a full working tool running

--- consensus_tool/consensus_genotyper.py
+++ consensus_tool/consensus_genotyper.py
@@ -22,13 +22,15 @@ def __main__():
   #outVcf.add_format(id="PL", number="G", type="Integer", description="GATK's normalized, Phred-scaled likelihoods for genotypes as defined in their VCF spec")
   outVcf.add_format(id="CN", number="1", type="Character", description="Consensus status")
   outVcf.add_format(id="GT", number="1", type="String", description="Genotype")
+  outVcf.add_info(id="X1", number="1", type="String", description="Placeholder for INFO parsing")
+  outVcf.add_info(id="X2", number="1", type="String", description="Placeholder 2 for INFO parsing")
   outVcf.samples = walker.samples
   outVcf.write_header()
 
 
   ## walk over each contig independently
-  #contigs = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY', 'chrMT']
   contigs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X'] 
+  #contigs = ['22']
 
   for contig in contigs:
     ## instantiate a walker over the input vcf files.
